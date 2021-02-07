a1206b88a9d6d8808f006be15daee8718dae2d08
Egor Sorokin <egor.sorokin@seznam.cz>
2021-02-07 19:43:57 +0100

    Deleting unnecessary comments

--- resources/js/actions/recipesListActions.js
+++ resources/js/actions/recipesListActions.js
@@ -15,18 +15,6 @@ export const addCommentary = (recipeId, text) => ({
     },
 });
 
-// export const fetchRecipes = () => {
-//     return (dispatch) => {
-//         return fetch("http://127.0.0.1:8000/api/recipes")
-//             .then((response) => response.json())
-//             .then((data) => {
-//                 console.log(data);
-//                 dispatch({ type: FETCH_RECIPES, payload: data.recipes });
-//             })
-//             .catch((err) => dispatch({ type: FETCH_ERROR }));
-//     };
-// };
-
 export const fetchRecipes = (currentLastId) => async (dispatch) => {
     const baseURL = window.location.origin;
     const response = await fetch(`${baseURL}/api/nextrecipes/${currentLastId}`);
