54ccc931f816922bd836d531f3f319c92cdfe8bd
Egor Sorokin <egor.sorokin@seznam.cz>
2021-02-14 16:51:06 +0100

    delete unnecessary CategoryChips.jsx, category selector on Recipes page, using combined reducer for fetching categories + recipes, applying new categories API

--- resources/js/pages/Recipes.jsx
+++ resources/js/pages/Recipes.jsx
@@ -1,14 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import RecipesList from '../components/RecipesList.jsx';
-import {
-    fetchRecipes,
-    fetchCategories,
-} from '../actions/recipesListActions.js';
-import { Container, Box, Typography, Chip } from '@material-ui/core';
+import { fetchRecipes } from '../actions/recipesListActions.js';
+import { fetchRecipesAndCategories } from '../actions/combinedActions.js';
+
+import { Container, Box, Typography, FormControl, InputLabel, Select } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 100,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
 
 const Recipes = () => {
     const dispatch = useDispatch();
+    const classes = useStyles();
+
     const recipesList = useSelector((state) => state.recipesObject.recipes);
     const currentLastId = useSelector(
         (state) => state.recipesObject.currentLastId
@@ -17,19 +29,32 @@ const Recipes = () => {
         (state) => state.recipesObject.isLastRecipes
     );
 
+    const categories = useSelector((state) => state.categories)
+
     const [previousLastId, setPreviousLastId] = useState(0);
+    const [currentCategory, setCurrentCategory] = useState('');
 
     useEffect(() => {
-        if (!recipesList.length) dispatch(fetchRecipes(currentLastId));
+        if (!recipesList.length) dispatch(fetchRecipesAndCategories(currentLastId)) //dispatch(fetchRecipes(currentLastId));
     }, [dispatch]);
 
     useEffect(() => {
         if (previousLastId !== 0) dispatch(fetchRecipes(currentLastId));
     }, [previousLastId]);
 
+    const handleChange = (e) => {
+        setCurrentCategory(e.target.value);
+    }
+
     const renderRecipes = () => {
         setPreviousLastId(currentLastId + 10);
     };
+
+    const renderCategoryOptions = (categories) => {
+        return categories.map(item => (
+            <option key={item.id} value={item.name}>{item.name}</option>
+        ))
+    }
 
     return (
         <Container maxWidth='lg'>
@@ -37,8 +62,18 @@ const Recipes = () => {
                 <Typography variant='h3'>Рецепты</Typography>
             </Box>
             <Box my={3}>
-                <Typography variant='h4'>Категории</Typography>
-                <Box></Box>
+                <FormControl className={classes.formControl}>
+                    <InputLabel htmlFor="age-native-simple">Категория</InputLabel>
+                    <Select
+                    native
+                    value={currentCategory}
+                    onChange={handleChange}
+                    className={classes.selectEmpty}
+                    >
+                        <option aria-label="None" value="" />
+                        {renderCategoryOptions(categories)}
+                    </Select>
+                </FormControl>
             </Box>
             {recipesList.length ? (
                 <RecipesList
