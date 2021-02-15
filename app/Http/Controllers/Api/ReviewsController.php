484ecaf15cf437aee1e300636428422770593fbf
Nikolai Antonov <nantonov@mail.ru>
2021-02-15 20:57:23 +0700

    Обновил роуты.
    Обновил роуты.

--- app/Http/Controllers/Api/ReviewsController.php
+++ app/Http/Controllers/Api/ReviewsController.php
@@ -19,16 +19,6 @@ class ReviewsController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -50,17 +40,6 @@ class ReviewsController extends Controller
     public function show($id)
     {
         return response()->json(['status' => true, 'data' => Reviews::find($id)]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
