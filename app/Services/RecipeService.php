6adfcfb5035d371f9190269eb6fbf8e2f7120334
Nikolai Antonov <nantonov@mail.ru>
2021-02-22 20:59:24 +0700

    Исправлен метод добавления рецепта.
    Исправлен метод добавления рецепта.
    
    В списке отзывов добавлена информация об ИД рецепта.

--- app/Services/RecipeService.php
+++ app/Services/RecipeService.php
@@ -19,7 +19,7 @@ class RecipeService
 
         $ingredients = DB::table('ingredients_in_recipes')->select('ingredients.name as ingredient_name', 'ingredients_in_recipes.count as count','units.name as unit_name','ingredients.product_fat as fat', 'ingredients.product_protein as protein', 'ingredients.product_carb as carb', 'ingredients.calorie','ingredients.id as ingredient_id')->where('ingredients_in_recipes.recipe_id','=',$id)->join('ingredients','ingredients_in_recipes.ingredient_id','=','ingredients.id')->join('units','ingredients.unit_id','=','units.id')->get();
 
-        $reviews = DB::table('reviews')->select('users.name as user_name','reviews.description','reviews.updated_at', 'users.id as user_id')->where('reviews.recipe_id','=',$id)->join('users','reviews.author_id','=','users.id')->get();
+        $reviews = DB::table('reviews')->select('users.name as user_name','reviews.description','reviews.updated_at', 'users.id as user_id', 'reviews.id as id')->where('reviews.recipe_id','=',$id)->join('users','reviews.author_id','=','users.id')->get();
 
         $steps = Step::where('recipe_id', $id)->get(['heading','image', 'description', 'step']);
 
